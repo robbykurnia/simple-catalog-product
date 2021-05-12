4b0891a7e58240758cbd7577d3e37a12491e6bd6
robbykurnia <roby.kurniaesaputra@gmail.com>
2021-05-13 00:45:18 +0700

    move non-pages from pages

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { useMemo } from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
-import reducers from '../pages/reducers';
+import reducers from '../components/reducers';
 
 let store;
 
